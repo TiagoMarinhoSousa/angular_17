8a21c5cd6c12517caab44a57839e045998cba9ff
TiagoMarinhoSousa <tiagomarinho9101@gmail.com>
2024-07-04 16:20:12 -0300

    property, event, atribute, css, style bindings

--- 4_projeto-binding/src/app/app.component.ts
+++ 4_projeto-binding/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InputType } from 'zlib';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,44 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+
+
   title = '4 - Projeto binding - diretivas - templates - decorators';
   inputText: string = "Texto inicial";
   inputType: string = "text";
   isDisabled: boolean = false;
+  inputResposta: InputType = ""
+  buttonTitle: string = "Texto do botão"
+  buttonDisable: boolean = false;
+
+  enableInput() {
+    this.isDisabled = false;
+  }
+
+  disableInput() {
+    this.isDisabled = true;
+  }
+
+  setTextTypeInput() {
+    this.inputType = "text";
+  }
+  setPasswordTypeInput() {
+    this.inputType = "password";
+  }
+
+  handleInputKeyup($event: KeyboardEvent) {
+    const currentText = (event?.target as HTMLInputElement).value;
+    console.log(currentText);
+    // const resposta = currentText;
+    // console.log(resposta);
+    this.inputResposta = currentText ;
+    console.log(this.inputResposta);
+  }
+
+  onButtonClick() {
+    this.buttonTitle = "Textto ALTERADO!!"
+    this.buttonDisable = !this.buttonDisable;
+  }
+
 }
